de122467cac0961c3d3e9926183eed400d5701b6
lucasnnobrega <lucasnnobrega@users.noreply.github.com>
2016-08-03 18:28:03 -0300

    Old bugs fixed, new bugs appear

--- Questoes/Questao 20/q20.c
+++ Questoes/Questao 20/q20.c
@@ -2,7 +2,28 @@
 #include <string.h>
 //#include <stdlib.h>
 
-void q20(int* controle)
+void qant_x_preco(float preco, int* total)
+{
+    int quantidade = 0;
+
+    printf("Diga a quantidade de produtos: \n");
+    scanf("%i", &quantidade);
+    printf("%i", quantidade);
+
+    printf("preco é: %f \n", preco);
+    printf("total parcial: %f \n \n", *total);
+    printf("endereço de total: %d", &total);
+
+    *total = *total + ((float)quantidade * preco);
+
+    printf("preco é: %f \n", preco);
+    printf("total parcial: %f \n \n", *total);
+    printf("endereço de total: %d", &total);
+
+
+}
+
+void q20(int* contrl)
 {
     static float total = 0;
 	char codigoproduto[4];
@@ -18,6 +39,7 @@ void q20(int* controle)
 	{
 		printf("Produto numero 1001 \n");
 		preco = 5.32;
+		printf("%i", &total);
 		qant_x_preco(preco, &total);
 
 	}
@@ -52,25 +74,17 @@ void q20(int* controle)
     }
     else if(strcmp(codigoproduto, "1234") == 0)
     {
-        printf("O valor total e: \n");
-        printf("%3.2f", total)
-        *controle = 0;
+        printf("O valor total é: \n");
+        printf("%3.2f \n", total);
+        printf("endereço de total: %d", &total);
+
+        *contrl = 0;
     }
     else
     {
-        printf("Código Errado");
+        printf("Código Errado. \n");
         goto pergunta;
     }
-
-}
-
-float qant_x_preco(float preco, int* total)
-{
-    int quantiddade;
-
-    printf("Diga a quantidade de produtos: \n");
-    scanf("%i", quantidade);
-    *total = *total + (quantidade * preco);
 
 }
 
@@ -83,7 +97,7 @@ int main ()
 
 	do{
         q20(&var);
-	}while(var)
+	}while(var);
 
 }
 
